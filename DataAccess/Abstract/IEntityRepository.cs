dfb520c039ff02adfaed8ac6010e616e68f8d4af
yigit <yigityalciin@hotmail.com>
2022-08-12 15:19:10 +0300

    12.08

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entitiy);
+        List<T> GetAllByCategory(int categoryId);
+
+    }
+}
