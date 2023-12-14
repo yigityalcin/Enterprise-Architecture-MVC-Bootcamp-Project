14bcc6816244b69ac92ccf5e1416c054f6352064
Yigit Yalcin <yigityalciin@hotmail.com>
2023-12-15 01:40:31 +0300

    Entity Framework

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -11,27 +13,51 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Product entity)
         {
-            throw new NotImplementedException();
+            //IDispossable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity);//referansı yakala 
+                addedEntity.State = EntityState.Added;// eklenecek bir nesne oldugunu setle
+                context.SaveChanges(); // hepsini gerceklestir ve ekle
+            }
         }
 
         public void Delete(Product entity)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);//referansı yakala 
+                deletedEntity.State = EntityState.Deleted;// silinecek bir nesne oldugunu setle
+                context.SaveChanges(); // hepsini gerceklestir ve sil
+            }
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
         {
-            throw new NotImplementedException();
+            //tek data getirir
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
         }
 
         public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            //birden cok data için
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList(); //Eğer filtre null ise select * from products çalıştır ve listeye ata eğer filtre null değilse filtreleyip listele
+            }
         }
 
         public void Update(Product entity)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity);//referansı yakala 
+                updatedEntity.State = EntityState.Modified;// güncellenecek bir nesne oldugunu setle
+                context.SaveChanges(); // hepsini gerceklestir ve güncelle
+            }
         }
     }
 }
