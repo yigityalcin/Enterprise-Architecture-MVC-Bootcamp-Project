ab0a1d9d18fd5435eada81d3e87fc8ae79a6b86f
Yigit Yalcin <yigityalciin@hotmail.com>
2023-12-19 03:49:42 +0300

    10.Ders İlk Bölüm

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,12 @@ namespace Business
 {
     public interface IProductService
     {
-        List<Product> GetAll();
+        List<Product> GetAll();//product list döndürüyor
         List<Product> GetAllByCategoryId(int id);//category idsine göre getiren kod
         List<Product> GetByUnitPrice(decimal min, decimal max);
-        List<ProductDetailDto> GetProductDetails();
+        List<ProductDetailDto> GetProductDetails();//list of product detail
+        Product GetById(int productId);//single product döndürür.
+        IResult Add(Product product);//herhangi bir şey döndürmüyor (void).
+
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.Utilities.Results;
+using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -15,9 +16,17 @@ namespace Business
         {
             _productDal = productDal;
         }
+
+        public IResult Add(Product product)
+        {
+            //business codes
+            _productDal.Add(product);
+            return new Result(true,"Ürün Eklendi");
+        }
+
         public List<Product> GetAll()
         {
-            //İş kodları
+            //business codes           
             //Yetkisi var mıı
             return _productDal.GetAll();
         }
@@ -25,6 +34,11 @@ namespace Business
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
