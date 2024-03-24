f1dee07954a1f21328ecd0c5916a8ad270ccb822
Yigit Yalcin <yigityalciin@hotmail.com>
2024-03-24 04:19:15 +0300

    feat: Validation tool added
    - ValidationTool class is added in Core Layer

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -23,14 +24,8 @@ namespace Business
         public IResult Add(Product product)
         {
             //business codes
-            //validation          
-            var context = new ValidationContext<Product>(product);
-            ProductValidator productValidator = new ProductValidator();
-            var result = productValidator.Validate(context);
-            if (!result.IsValid)
-            {
-                throw new ValidationException(result.Errors);
-            }
+            //validation                     
+            ValidationTool.Validate(new ProductValidator(), product);
 
             _productDal.Add(product);
             return new Result(true, Messages.ProductAdded);
