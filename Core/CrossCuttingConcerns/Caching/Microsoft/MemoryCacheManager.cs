e3863b329aede1855a439c5b2d1cb4af9a366467
Yigit Yalcin <yigityalciin@hotmail.com>
2024-07-15 00:55:51 +0300

    Lesson 15 02:05:50

--- Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
+++ Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -1,9 +1,11 @@
 ﻿using Core.Utilities.IoC;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Core.CrossCuttingConcerns.Caching.Microsoft;
@@ -45,6 +47,22 @@ public class MemoryCacheManager : ICacheManager
 
     public void RemoveByPattern(string pattern)
     {
-        throw new NotImplementedException();
+        var cacheEntriesCollectionDefinition = typeof(MemoryCache).GetProperty("EntriesCollection", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var cacheEntriesCollection = cacheEntriesCollectionDefinition.GetValue(_memoryCache) as dynamic;
+        List<ICacheEntry> cacheCollectionValues = new List<ICacheEntry>();
+
+        foreach (var cacheItem in cacheEntriesCollection)
+        {
+            ICacheEntry cacheItemValue = cacheItem.GetType().GetProperty("Value").GetValue(cacheItem, null);
+            cacheCollectionValues.Add(cacheItemValue);
+        }
+
+        var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        var keysToRemove = cacheCollectionValues.Where(d => regex.IsMatch(d.Key.ToString())).Select(d => d.Key).ToList();
+
+        foreach (var key in keysToRemove)
+        {
+            _memoryCache.Remove(key);
+        }
     }
 }
