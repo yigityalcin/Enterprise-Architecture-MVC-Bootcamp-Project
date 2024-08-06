5c1e4bbc6e42d880327df6c196c412b471b5dc6a
Yigit Yalcin <yigityalciin@hotmail.com>
2024-08-06 16:00:06 +0300

    CategoriesController api added to webapi

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -4,55 +4,57 @@ using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebAPI.Controllers
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    //Loosely coupled
+    //Dependency injection
+    //Ioc Container -- Inversion of Control
+    IProductService _productService; //naming convention _productService
+
+    public ProductsController(IProductService productService)
     {
-        //Loosely coupled
-        //Dependency injection
-        //Ioc Container -- Inversion of Control
-        IProductService _productService; //naming convention _productService
+        _productService = productService;
+    }
 
-        public ProductsController(IProductService productService)
-        {
-            _productService = productService;
-        }
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        //Swagger 
+        //Dependency chain
 
-        [HttpGet("getall")]
-        public IActionResult GetAll()
-        {
-            //Swagger 
-            //Dependency chain
-            var result = _productService.GetAll();
-            if (result.Success)
-            {
-                return Ok(result);
-            }
-            return BadRequest(result);
-        }
+        Thread.Sleep(1000);
 
-        [HttpGet("getbyid")]
-        public IActionResult GetById(int id)
+        var result = _productService.GetAll();
+        if (result.Success)
         {
-            var result = _productService.GetById(id);
-            if (result.Success)
-            {
-                return Ok(result);
-            }
-            return BadRequest(result);
+            return Ok(result);
         }
+        return BadRequest(result);
+    }
 
-        [HttpPost("add")]
-        public IActionResult Add(Product product)
+    [HttpGet("getbyid")]
+    public IActionResult GetById(int id)
+    {
+        var result = _productService.GetById(id);
+        if (result.Success)
         {
-            var result = _productService.Add(product);
-            if (result.Success)
-            {
-                return Ok(result);
-            }
-            return BadRequest(result);
+            return Ok(result);
         }
+        return BadRequest(result);
+    }
+
+    [HttpPost("add")]
+    public IActionResult Add(Product product)
+    {
+        var result = _productService.Add(product);
+        if (result.Success)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
     }
 }
